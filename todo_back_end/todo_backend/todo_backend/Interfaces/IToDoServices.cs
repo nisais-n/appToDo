55afde060f72467ad3b99d3aa30e5f45f95ddc7e
nisais-n <n.nisais@gmail.com>
2022-09-20 07:25:17 +0530

    completedBE

---
+++ todo_back_end/todo_backend/todo_backend/Interfaces/IToDoServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ToDos.BusinessObjects;
+
+namespace ToDos.Interfaces
+{
+    public interface IToDoServices
+    {
+        List<ToDo> GetToDos();
+        ToDo AddToDo(ToDo todo);
+        ToDo GetToDo(string id);
+        ToDo UpdateToDo(ToDo todo);
+         
+    }
+}
+
---
+++ todo_back_end/todo_backend/todo_backend/ToDo.BusinessObjects/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace ToDos.BusinessObjects
+{
+    public class ToDo
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? _id { get; set; }
+
+        public int  id { get; set; }
+
+        public string? toDoItemDescription { get; set; }
+
+        public bool isCompleted { get; set; }
+
+        public bool isImportant { get; set; }
+
+        public bool isDeleted { get; set; }
+    }
+}
+
